62189b3d40727f19b0c36b3db511557a5e697e77
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-04-17 21:57:11 +0200

    feat(treesitter): check for "MISSING" nodes and update test

--- src/dataset/restructure/treesitter.py
+++ src/dataset/restructure/treesitter.py
@@ -1,6 +1,5 @@
 import re
 from collections.abc import Generator
-from typing import cast
 
 from tree_sitter import Language, Node, Parser, Tree, TreeCursor
 from tree_sitter_language_pack import (SupportedLanguage, get_language,
@@ -35,13 +34,21 @@ class TreeSitter:
 
         comments: list[bytes] = [
             self._assert_not_none(attribute=cmnt.text, attr_name="cmnt.text")
-            # assert cmnt.text is not None
-            # cmnt.text
             for cmnt in self.traverse_tree(tree=self.tree)
             if cmnt.type == "comment"
         ]
 
         return comments
+
+    def check_missing_nodes(self) -> tuple[bool, str]:
+        if not (self._is_parsed or self._is_encoded):
+            raise Exception("Missing call to parse function! Abort...")
+
+        for node in self.traverse_tree(tree=self.tree):
+            if node.is_missing:
+                return (True, f"{node}")
+
+        return (False, "")
 
     def extract_directives(self) -> list[bytes]:
         if not (self._is_parsed or self._is_encoded):
@@ -49,16 +56,15 @@ class TreeSitter:
 
         directives: list[bytes] = [
             self._assert_not_none(attribute=drctv.text, attr_name="drctv.text")
-            # cast(bytes, drctv.text)
             for drctv in self.traverse_tree(tree=self.tree)
             if (
                 drctv.type == "preproc_def"
+                or drctv.type == "preproc_function_def"
                 or drctv.type == "preproc_if"
                 or drctv.type == "preproc_ifdef"
                 or drctv.type == "preproc_directive"
-                or drctv.type == "preproc_else"
-                or drctv.type == "preproc_elif"
-                or drctv.type == "preproc_function_def"
+                # or drctv.type == "preproc_else"
+                # or drctv.type == "preproc_elif"
             )
         ]
 
@@ -99,12 +105,21 @@ class TreeSitter:
         return attribute
 
 
+def read_file_content_as_str(filepath: str) -> str:
+    with open(file=filepath, mode="rb") as f:
+        file_content: bytes = f.read()
+
+    return file_content.decode(encoding="utf-8")
+
+
 def test():
     ts: TreeSitter = TreeSitter()
 
-    code = 'static int read_and_execute(bool interactive)\n{\n#if defined(_WIN32)\n String tmpbuf;\n String buffer;\n#endif\n\n /*\n line can be allocated by:\n - batch_readline. Use my_free()\n - my_win_console_readline. Do not free, see tmpbuf.\n - readline. Use free()\n */\n char *line= NULL;\n char in_string=0;\n ulong line_number=0;\n bool ml_comment= 0; \n COMMANDS *com;\n ulong line_length= 0;\n status.exit_status=1;\n\n real_binary_mode= !interactive && opt_binary_mode;\n for (;;)\n {\n /* Reset as SIGINT has already got handled. */\n sigint_received= 0;\n\n if (!interactive)\n {\n /*\n batch_readline can return 0 on EOF or error.\n In that case, we need to double check that we have a valid\n line before actually setting line_length to read_length.\n */\n line= batch_readline(status.line_buff, real_binary_mode);\n if (line) \n {\n line_length= status.line_buff->read_length;\n\n /*\n ASCII 0x00 is not allowed appearing in queries if it is not in binary\n mode.\n */\n if (!real_binary_mode && strlen(line) != line_length)\n {\n status.exit_status= 1;\n String msg;\n msg.append("ASCII \'\\\\0\' appeared in the statement, but this is not "\n "allowed unless option --binary-mode is enabled and mysql is "\n "run in non-interactive mode. Set --binary-mode to 1 if ASCII "\n "\'\\\\0\' is expected. Query: \'");\n msg.append(glob_buffer);\n msg.append(line);\n msg.append("\'.");\n put_info(msg.c_ptr(), INFO_ERROR);\n break;\n }\n\n /*\n Skip UTF8 Byte Order Marker (BOM) 0xEFBBBF.\n Editors like "notepad" put this marker in\n the very beginning of a text file when\n you save the file using "Unicode UTF-8" format.\n */\n if (!line_number &&\n (uchar) line[0] == 0xEF &&\n (uchar) line[1] == 0xBB &&\n (uchar) line[2] == 0xBF)\n {\n line+= 3;\n // decrease the line length accordingly to the 3 bytes chopped\n line_length -=3;\n }\n }\n line_number++;\n if (!glob_buffer.length())\n status.query_start_line=line_number;\n }\n else\n {\n char *prompt= (char*) (ml_comment ? " /*> " :\n glob_buffer.is_empty() ? construct_prompt() :\n !in_string ? " -> " :\n in_string == \'\\\'\' ?\n " \'> " : (in_string == \'`\' ?\n " `> " :\n " \\"> "));\n if (opt_outfile && glob_buffer.is_empty())\n fflush(OUTFILE);\n\n#if defined(_WIN32)\n size_t nread;\n tee_fputs(prompt, stdout);\n if (!tmpbuf.is_alloced())\n tmpbuf.alloc(65535);\n tmpbuf.length(0);\n buffer.length(0);\n line= my_win_console_readline(charset_info,\n (char *) tmpbuf.ptr(),\n tmpbuf.alloced_length(),\n &nread);\n if (line && (nread == 0))\n {\n tee_puts("^C", stdout);\n reset_prompt(&in_string, &ml_comment);\n continue;\n }\n else if (*line == 0x1A) /* (Ctrl + Z) */\n break;\n#else\n if (opt_outfile)\n fputs(prompt, OUTFILE);\n /*\n free the previous entered line.\n */\n if (line)\n free(line);\n line= readline(prompt);\n\n if (sigint_received)\n {\n sigint_received= 0;\n tee_puts("^C", stdout);\n reset_prompt(&in_string, &ml_comment);\n continue;\n }\n#endif /* defined(_WIN32) */\n /*\n When Ctrl+d or Ctrl+z is pressed, the line may be NULL on some OS\n which may cause coredump.\n */\n if (opt_outfile && line)\n fprintf(OUTFILE, "%s\\n", line);\n\n line_length= line ? strlen(line) : 0;\n }\n // End of file or system error\n if (!line)\n {\n if (status.line_buff && status.line_buff->error)\n status.exit_status= 1;\n else\n status.exit_status= 0;\n break;\n }\n\n /*\n Check if line is a mysql command line\n (We want to allow help, print and clear anywhere at line start\n */\n if ((named_cmds || glob_buffer.is_empty())\n && !ml_comment && !in_string && (com= find_command(line)))\n {\n if ((*com->func)(&glob_buffer,line) > 0)\n {\n // lets log the exit/quit command.\n if (interactive && status.add_to_history && com->cmd_char == \'q\')\n add_filtered_history(line);\n break;\n }\n if (glob_buffer.is_empty()) // If buffer was emptied\n in_string=0;\n if (interactive && status.add_to_history)\n add_filtered_history(line);\n continue;\n }\n if (add_line(glob_buffer, line, line_length, &in_string, &ml_comment,\n status.line_buff ? status.line_buff->truncated : 0))\n break;\n }\n /* if in batch mode, send last query even if it doesn\'t end with \\g or go */\n\n if (!interactive && !status.exit_status)\n {\n remove_cntrl(glob_buffer);\n if (!glob_buffer.is_empty())\n {\n status.exit_status=1;\n if (com_go(&glob_buffer,line) <= 0)\n status.exit_status=0;\n }\n }\n\n#if defined(_WIN32)\n buffer.free();\n tmpbuf.free();\n#else\n if (interactive)\n /*\n free the last entered line.\n */\n free(line);\n#endif\n\n /*\n If the function is called by \'source\' command, it will return to interactive\n mode, so real_binary_mode should be FALSE. Otherwise, it will exit the\n program, it is safe to set real_binary_mode to FALSE.\n */\n real_binary_mode= FALSE;\n return status.exit_status;\n}}'
+    code = read_file_content_as_str(filepath="tmp.c")
+    print(code)
 
     ts.parse_input(code_snippet=code)
+    ts.check_missing_nodes()
     comments: list[bytes] = ts.extract_comments()
     directives: list[bytes] = ts.extract_directives()
 
