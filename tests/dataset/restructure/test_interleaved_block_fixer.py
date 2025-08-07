470abdd62ba7730319ab45bb559cc264dc151d54
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-07 12:11:01 +0200

    test(test_interleaved_block_fixer): add real-like long input

--- tests/dataset/restructure/test_interleaved_block_fixer.py
+++ tests/dataset/restructure/test_interleaved_block_fixer.py
@@ -114,19 +114,227 @@ class TestRestructurerUnit:
 
     # ---
     INTERLEAVED_IF_MULTIPLE_OPENERS_ONE_CLOSER_INPUT = """
-      ssize_t sys_recvfrom(int s, void *buf, size_t len, int flags, struct sockaddr *from, socklen_t *fromlen) {
-        ssize_t ret = 1;
-      #if defined(EWOULDBLOCK)
-        if(ret > 1) {
-      #elif defined(EWOULDBLOCK)
-        if(ret < 1) {
-      #else
-        if(ret == 1) {
-      #endif
-          printf("this is another complex scenario where multiple openers and one closer");
-        }
+    ssize_t sys_recvfrom(int s, void *buf, size_t len, int flags, struct sockaddr *from, socklen_t *fromlen) {
+      ssize_t ret = 1;
+    #if defined(EWOULDBLOCK)
+      if(ret > 1) {
+    #elif defined(EWOULDBLOCK)
+      if(ret < 1) {
+    #else
+      if(ret == 1) {
+    #endif
+        printf("this is another complex scenario where multiple openers and one closer");
+    }
     """
+    # ---
 
+    # ---
+    LONG_PROBLEMATIC_REAL_INPUT = """
+    unpack_Z_stream(int fd_in, int fd_out) {
+        IF_DESKTOP(long long total_written = 0;)
+        IF_DESKTOP(long long) int retval = -1;
+        unsigned char *stackp;
+        long code;
+        int finchar;
+        long oldcode;
+        long incode;
+        int inbits;
+        int posbits;
+        int outpos;
+        int insize;
+        int bitmask;
+        long free_ent;
+        long maxcode;
+        long maxmaxcode;
+        int n_bits;
+        int rsize = 0;
+        unsigned char *inbuf;
+        unsigned char *outbuf;
+        unsigned char *htab;
+        unsigned short *codetab;
+
+        int maxbits;
+
+        int block_mode;
+
+        inbuf = xzalloc(IBUFSIZ + 64);
+        outbuf = xzalloc(OBUFSIZ + 2048);
+        htab = xzalloc(HSIZE);
+        codetab = xzalloc(HSIZE * sizeof(codetab[0]));
+
+        insize = 0;
+
+        if(full_read(fd_in, inbuf, 1) != 1) {
+          bb_error_msg("short read");
+          goto err;
+        }
+
+        maxbits = inbuf[0] & BIT_MASK;
+        block_mode = inbuf[0] & BLOCK_MODE;
+        maxmaxcode = MAXCODE(maxbits);
+
+        if(maxbits > BITS) {
+          bb_error_msg("compressed with %d bits, can only handle " BITS_STR " bits", maxbits);
+          goto err;
+        }
+
+        n_bits = INIT_BITS;
+        maxcode = MAXCODE(INIT_BITS) - 1;
+        bitmask = (1 << INIT_BITS) - 1;
+        oldcode = -1;
+        finchar = 0;
+        outpos = 0;
+        posbits = 0 << 3;
+
+        free_ent = ((block_mode) ? FIRST : 256);
+
+        for(code = 255; code >= 0; --code) { tab_suffixof(code) = (unsigned char)code; }
+
+        do {
+        resetbuf: {
+          int i;
+          int e;
+          int o;
+
+          o = posbits >> 3;
+          e = insize - o;
+
+          for(i = 0; i < e; ++i) inbuf[i] = inbuf[i + o];
+
+          insize = e;
+          posbits = 0;
+        }
+
+          if(insize < (int)(IBUFSIZ + 64) - IBUFSIZ) {
+            rsize = safe_read(fd_in, inbuf + insize, IBUFSIZ);
+
+            insize += rsize;
+          }
+
+          inbits = ((rsize > 0) ? (insize - insize % n_bits) << 3 : (insize << 3) - (n_bits - 1));
+
+          while(inbits > posbits) {
+            if(free_ent > maxcode) {
+              posbits = ((posbits - 1) + ((n_bits << 3) - (posbits - 1 + (n_bits << 3)) % (n_bits << 3)));
+              ++n_bits;
+              if(n_bits == maxbits) {
+                maxcode = maxmaxcode;
+              } else {
+                maxcode = MAXCODE(n_bits) - 1;
+              }
+              bitmask = (1 << n_bits) - 1;
+              goto resetbuf;
+            }
+            {
+              unsigned char *p = &inbuf[posbits >> 3];
+
+              code = ((((long)(p[0])) | ((long)(p[1]) << 8) | ((long)(p[2]) << 16)) >> (posbits & 0x7)) &
+                     bitmask;
+            }
+            posbits += n_bits;
+
+            if(oldcode == -1) {
+              oldcode = code;
+              finchar = (int)oldcode;
+              outbuf[outpos++] = (unsigned char)finchar;
+              continue;
+            }
+
+            if(code == CLEAR && block_mode) {
+              clear_tab_prefixof();
+              free_ent = FIRST - 1;
+              posbits = ((posbits - 1) + ((n_bits << 3) - (posbits - 1 + (n_bits << 3)) % (n_bits << 3)));
+              n_bits = INIT_BITS;
+              maxcode = MAXCODE(INIT_BITS) - 1;
+              bitmask = (1 << INIT_BITS) - 1;
+              goto resetbuf;
+            }
+
+            incode = code;
+            stackp = de_stack;
+
+            if(code >= free_ent) {
+              if(code > free_ent) {
+                unsigned char *p;
+
+                posbits -= n_bits;
+                p = &inbuf[posbits >> 3];
+
+                bb_error_msg("insize:%d posbits:%d inbuf:%02X %02X %02X %02X %02X (%d)", insize, posbits,
+                             p[-1], p[0], p[1], p[2], p[3], posbits & 07);
+                bb_error_msg("corrupted data");
+                goto err;
+              }
+
+              *--stackp = (unsigned char)finchar;
+              code = oldcode;
+            }
+
+            while((long)code >= (long)256) {
+              *--stackp = tab_suffixof(code);
+              code = tab_prefixof(code);
+            }
+
+            finchar = tab_suffixof(code);
+            *--stackp = (unsigned char)finchar;
+
+            {
+              int i;
+
+              i = de_stack - stackp;
+              if(outpos + i >= OBUFSIZ) {
+                do {
+                  if(i > OBUFSIZ - outpos) { i = OBUFSIZ - outpos; }
+
+                  if(i > 0) {
+                    memcpy(outbuf + outpos, stackp, i);
+                    outpos += i;
+                  }
+
+                  if(outpos >= OBUFSIZ) {
+                    full_write(fd_out, outbuf, outpos);
+
+                    IF_DESKTOP(total_written += outpos;)
+                    outpos = 0;
+                  }
+                  stackp += i;
+                  i = de_stack - stackp;
+                } while(i > 0);
+              } else {
+                memcpy(outbuf + outpos, stackp, i);
+                outpos += i;
+              }
+            }
+
+            code = free_ent;
+            if(code < maxmaxcode) {
+              tab_prefixof(code) = (unsigned short)oldcode;
+              tab_suffixof(code) = (unsigned char)finchar;
+              free_ent = code + 1;
+            }
+
+            oldcode = incode;
+          }
+
+        } while(rsize > 0);
+
+        if(outpos > 0) {
+          full_write(fd_out, outbuf, outpos);
+
+          IF_DESKTOP(total_written += outpos;)
+        }
+
+        retval = IF_DESKTOP(total_written) + 0;
+
+    err:
+      free(inbuf);
+      free(outbuf);
+      free(htab);
+      free(codetab);
+      return retval;
+    }
+    """
+    LONG_PROBLEMATIC_REAL_OUTPUT = LONG_PROBLEMATIC_REAL_INPUT
     # ---
 
     interleaved_dowhile_test_cases = [
@@ -196,7 +404,6 @@ class TestRestructurerUnit:
     # ==============================================================================================================
     
     # --- 
-    # input previously defined in do_while test
     INTERLEAVED_IF_MULTIPLE_OPENERS_ONE_CLOSER_OUPUT = """
     ssize_t sys_recvfrom(int s, void *buf, size_t len, int flags, struct sockaddr *from, socklen_t *fromlen) {
       ssize_t ret = 1;
@@ -209,11 +416,10 @@ class TestRestructurerUnit:
     #endif
     """
     # --- 
-    
+
     # ---
     NO_INTERLEAVED_STATEMENTS_INPUT = """
-    ssize_t sys_recvfrom(int s, void *buf, size_t len, int flags, struct sockaddr *from,
-                         socklen_t *fromlen) {
+    ssize_t sys_recvfrom(int s, void *buf, size_t len, int flags, struct sockaddr *from, socklen_t *fromlen) {
       ssize_t ret = 1;
     #if defined(EWOULDBLOCK)
       if(ret > 1) { printf("this is another complex scenario where multiple openers and one closer"); }
@@ -222,7 +428,6 @@ class TestRestructurerUnit:
     #else
       if(ret == 1) { printf("this is another complex scenario where multiple openers and one closer"); }
     #endif
-    }
     """
     NO_INTERLEAVED_STATEMENTS_OUTPUT = NO_INTERLEAVED_STATEMENTS_INPUT
     # ---
@@ -446,10 +651,9 @@ class TestRestructurerUnit:
     """
     # ---
 
-
     full_pipeline_test_cases = [
-        pytest.param(INTERLEAVED_IF_MULTIPLE_OPENERS_ONE_CLOSER_INPUT, INTERLEAVED_IF_MULTIPLE_OPENERS_ONE_CLOSER_OUPUT, "c", id="interleaved_if_multiple_openers"),
         pytest.param(NO_INTERLEAVED_STATEMENTS_INPUT, NO_INTERLEAVED_STATEMENTS_OUTPUT, "c", id="no_interleaved_statements"),
+        pytest.param(INTERLEAVED_IF_MULTIPLE_OPENERS_ONE_CLOSER_INPUT, INTERLEAVED_IF_MULTIPLE_OPENERS_ONE_CLOSER_OUPUT, "c", id="interleaved_if_multiple_openers"),
         pytest.param(BROKEN_INTERLEAVED_IF_ELIF_ELSE_ENDIF_INPUT, BROKEN_INTERLEAVED_IF_ELIF_ELSE_ENDIF_OUTPUT, "c", id="interleaved_if_elif_else_endif"),
         pytest.param(BROKEN_SIMPLE_INTERLEAVED_SWITCH_INPUT, BROKEN_SIMPLE_INTERLEAVED_SWITCH_OUTPUT, "c", id="interleaved_switch_simple"),
         pytest.param(BROKEN_NESTED_INTERLEAVED_SWITCH_INPUT, BROKEN_NESTED_INTERLEAVED_SWITCH_OUTPUT, "c", id="interleaved_switch"),
@@ -458,6 +662,7 @@ class TestRestructurerUnit:
         pytest.param(BROKEN_INTERLEAVED_SIMPLE_FOR_LOOP_INPUT, BROKEN_INTERLEAVED_SIMPLE_FOR_LOOP_OUTPUT, "c", id="interleaved_simple_for"),
         pytest.param(BROKEN_INTERLEAVED_FOR_LOOP_INPUT, BROKEN_INTERLEAVED_FOR_LOOP_OUTPUT, "c", id="interleaved_for"),
         pytest.param(BROKEN_INTERLEAVED_FOR_CONTENT_INPUT, BROKEN_INTERLEAVED_FOR_CONTENT_OUTPUT, "c", id="interleaved_content_for_loop"),
+        pytest.param(LONG_PROBLEMATIC_REAL_INPUT, LONG_PROBLEMATIC_REAL_OUTPUT, "c", id="problematic_long_real_testcase"),
     ]
     @pytest.mark.parametrize("input, output, lang", full_pipeline_test_cases)
     def test_interleaved(self, interleaved_block_fixer: InterleavedBlockFixer, input: str, output: str, lang: str):
