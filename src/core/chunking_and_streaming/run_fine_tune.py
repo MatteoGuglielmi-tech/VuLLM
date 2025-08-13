7012a4cb66b1de1e138885fff241b9d0d74e4146
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2025-08-13 11:52:22 +0200

    refactor(main): separate train and val from test due to oom

---
+++ src/core/chunking_and_streaming/run_fine_tune.py
@@ -0,0 +1,48 @@
+from argparse import Namespace
+
+from core.chunking_and_streaming.shared.argparser import get_parser
+from core.chunking_and_streaming.unsloth_fine_tuner import UnslothFineTunePipeline
+
+from core.chunking_and_streaming.dataset import DatasetHandler
+from transformers import AutoTokenizer
+
+
+BASE_MODEL = "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit"
+
+
+if __name__ == "__main__":
+    args: Namespace = get_parser().parse_args()
+
+    print("--- 🚀 Starting Data Preparation Process ---")
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=BASE_MODEL)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "right"
+
+    # split data
+    data_handler = DatasetHandler(tokenizer=tokenizer, max_chunk_tokens=args.max_seq_length)
+
+    # --- Caching Logic ---
+    if data_handler.pth_tokenized_train.exists() and data_handler.pth_tokenized_val.exists():
+        print("✅ Pre-tokenized data found. Loading from disk.")
+        tokenized_train_val_chunks = data_handler.load_tokenized_dataset()
+    else:
+        print("Pre-tokenized data not found. Running full data preparation pipeline...")
+        data_handler.execute_base()
+        tokenized_train_val_chunks = data_handler.tokenize_chunks_train()
+        data_handler.cleanup_intermediate_files()
+
+    if not tokenized_train_val_chunks: raise RuntimeError("Data processing failed. No tokenized data was produced.")
+
+    print("✅ Data preparation complete.")
+
+    print("\n--- 🚀 Starting Training Process ---")
+    training_pipeline = UnslothFineTunePipeline(
+        hf_train_data=processed_data_splits["train"], # type: ignore
+        hf_eval_data=processed_data_splits["val"],  # type: ignore
+        base_model_str=BASE_MODEL,
+        max_seq_length=args.max_seq_length,
+        training_epochs=1,
+        # training_steps=100,  # train for 100 steps
+    )
+    training_pipeline.execute()
+    print("\n--- ✅ Training complete. ---")
---
+++ src/core/chunking_and_streaming/run_test.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from argparse import Namespace
+from core.chunking_and_streaming.shared.argparser import get_parser
+from core.chunking_and_streaming.unsloth_test import InferencePipeline
+
+from core.chunking_and_streaming.dataset import DatasetHandler
+from transformers import AutoTokenizer
+from .run_fine_tune import BASE_MODEL
+
+
+if __name__ == "__main__":
+    args: Namespace = get_parser().parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=BASE_MODEL)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "right"
+
+    # split data
+    data_handler = DatasetHandler(tokenizer=tokenizer, max_chunk_tokens=args.max_seq_length)
+    data_handler.execute_base()
+    tokenized_train_val_chunks = data_handler.tokenize_chunks_train()
+
+    print("\n--- 🚀 Starting Evaluation Process ---")
+    # load the chunked (but not tokenized) test data for evaluation
+    df_test = pd.read_json(path_or_buf=data_handler.pth_final_test, lines=True)
+    inference_pipeline = InferencePipeline(lora_model_dir=args.lora_model_dir, max_seq_length=args.max_seq_length)
+    evaluation_results_df = inference_pipeline.evaluate_on_test_set(df_test_data=df_test)
+
+    InferencePipeline.calculate_and_save_metrics(
+        y_true=evaluation_results_df["ground_truth"].to_list(),
+        y_pred=evaluation_results_df["predicted_label"].to_list(),
+        output_dir=inference_pipeline.output_dir,
+    )
+
+    print(f"\n🎉 Pipeline complete! All evaluation artifacts saved in: {inference_pipeline.output_dir}")
