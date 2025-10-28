b119e7d6a6d76fddfd465c665f5a25ee9c760af8
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-10-28 12:04:36 +0100

    build(triton): remove programmatic setup

--- src/core/cot_training/utilities/__init__.py
+++ src/core/cot_training/utilities/__init__.py
@@ -1,7 +1,6 @@
 from .detection import is_main_process
 from .decorators import main_process_only
 from .utils import rich_table, rich_print, rich_rule, rich_exception, get_instruction_response_parts, cleanup_resources
-from .setup_triton import setup_triton_cache
 
 
 __all__ = [
@@ -16,6 +15,4 @@ __all__ = [
     "rich_exception",
     "cleanup_resources",
     "get_instruction_response_parts",
-    #setup_triton
-    "setup_triton_cache"
 ]
